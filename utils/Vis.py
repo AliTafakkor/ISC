967bc155312ac535bd79f85873aa32a58fd95b84
Ali <alitafakkor@gmail.com>
2023-11-13 17:41:44 -0500

    added range option to plot function

--- utils/Vis.py
+++ utils/Vis.py
@@ -1,7 +1,7 @@
 from surfplot import Plot
 import os
 
-def plot_maps(map):
+def plot_maps(map, range=None):
     # Surface mesh files
     surf_mesh = os.path.join('resources','surface_meshes','fs_LR_32K','fs_LR.32k.{hemi}.very_inflated.surf.gii')
 
@@ -14,7 +14,7 @@ def plot_maps(map):
     # plot surface mesh
     p = Plot(surf_lh=surf_mesh.format(hemi='L'), surf_rh=surf_mesh.format(hemi='R'), brightness = 0.7, size=(1600, 400), zoom=1.2, layout='row')
     # plot the map
-    p.add_layer({'left': map.format(hemi='L'), 'right': map.format(hemi='R')}, cbar=True)
+    p.add_layer({'left': map.format(hemi='L'), 'right': map.format(hemi='R')}, cbar=True, color_range=range)
     # plot atlas borders
     p.add_layer({'left': surf_atlas.format(hemi='L'), 'right': surf_atlas.format(hemi='R')}, cmap='gray', as_outline=True, cbar=False)
     fig = p.build(cbar_kws=kws)
