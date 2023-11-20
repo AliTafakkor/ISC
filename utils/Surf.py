5af1e300b7672610baaa1e7af64cbc6e6b79e249
Ali <alitafakkor@gmail.com>
2023-11-20 16:54:57 -0500

    added vertex2gii function

--- utils/Surf.py
+++ utils/Surf.py
@@ -24,4 +24,16 @@ def roi2gii(roi_data, fpath, fname):
         nib.save(gii_image, os.path.join(fpath,f'{fname}_{hemi}.shape.gii'))
 
     return surface_values
+
+def vertex2gii(vertex_data, fpath, fname):
+    """Create GIFTI Image from ROI values and save it in the results."""
+    # Iterate on Left and Right hemispheres
+    for hemi in ['L', 'R']:
+        # Create a GIFTI data array for the map
+        gii_darray = nib.gifti.GiftiDataArray(data=vertex_data[hemi], intent=nib.nifti1.intent_codes['NIFTI_INTENT_SHAPE'])
+        # Create a GIFTI image for the map
+        gii_image = nib.gifti.GiftiImage(darrays=[gii_darray])
+        # Save GIFTI image
+        nib.save(gii_image, os.path.join(fpath,f'{fname}_{hemi}.shape.gii'))
+
     
